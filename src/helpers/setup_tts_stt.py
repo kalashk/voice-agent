9e68beb6bbe4b213fc1f05aed7be136ad02d37ad
kalashk <jaiswal18kalash@gmail.com>
2025-10-03 11:55:30 +0530

    update

--- src/helpers/setup_tts_stt.py
+++ src/helpers/setup_tts_stt.py
@@ -66,7 +66,7 @@ def setup_stt(provider: str = STT_PROVIDER):
 
 def setup_llm(provider: str = LLM_PROVIDER):
     if provider == "openai":
-        return openai.LLM(model="gpt-4o-mini")
+        return openai.LLM(model="gpt-5o-mini")
     elif provider == "gemini":
         return google.LLM(model="gemini-2.5-flash-preview-05-20")
     else:
